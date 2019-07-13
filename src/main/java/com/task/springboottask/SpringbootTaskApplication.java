1906edf0a3d23cd7bb9ec74fc1c52fc376eb3da7
yueyan123 <yuey5250@163.com>
2019-07-13 12:35:39 +0800

    '初次代码提交'

---
+++ src/main/java/com/task/springboottask/SpringbootTaskApplication.java
@@ -0,0 +1,14 @@
+package com.task.springboottask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class SpringbootTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTaskApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/task/springboottask/controller/CronMapper.java
@@ -0,0 +1,10 @@
+package com.task.springboottask.controller;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface CronMapper {
+    @Select("select cron from cron limit 1")
+    String getCron();
+}
